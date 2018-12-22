fd617f4f417fb7400d1273b6ec2f25761162cc3f
Vasyl Vasylyk <vasiliyworkplace@gmail.com>
2018-12-23 00:06:27 +0100

    add leaflet

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-createMap();
+var map = L.map('map').setView([50.062501, 19.939354], 15);
 
-function createMap(){
-	var mapOptions = {
-		zoom: 2,
-		center: new google.maps.LatLng(0, 0)
-	};
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmFzaWxpeXdvcmtwbGFjZSIsImEiOiJjanEwMXJwOW0waTR4NDNvZHN3ZTY0dHEwIn0.6BsJwTlPJSHObAtL_ZPMSg', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox.streets'
+}).addTo(map);
